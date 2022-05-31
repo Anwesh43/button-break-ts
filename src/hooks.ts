729d41b1eb4163ade7f3590a2dd3d6e2da5195ac
Anwesh43 <anweshthecool0@gmail.com>
2022-05-31 11:39:47 +0530

    adding hooks

---
+++ src/hooks.ts
@@ -0,0 +1,73 @@
+import {useState, useEffect, CSSProperties} from 'react'
+
+const scGap : number = 0.01 
+const delay : number = 20 
+
+export const useAnimatedScale = () => {
+    const [scale, setScale] = useState(0)
+    const [animated, setAnimated] = useState(false)
+
+    return {
+        scale, 
+        start() {
+            if (!animated) {
+                setAnimated(true)
+                const interval = setInterval(() => {
+                    setScale((prev : number) => {
+                        if (prev > 1) {
+                            setAnimated(false)
+                            clearInterval(interval)
+                            return 0
+                        }
+                        return prev + scGap 
+                    })
+                }, delay)
+            }
+        }
+    }
+}
+
+export const useDimension = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        window.onresize = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        return () => {
+            window.onresize = () => {
+
+            }
+        }
+    })
+    return {
+        w, 
+        h
+    }
+}
+
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const sf : number = sinify(scale)
+    const position = 'absolute'
+    const size : number = 6.9 
+    const background : string = "green"
+    return {
+        blockStyle(i : number) : CSSProperties {
+            const width = `${size}px`
+            const height = `${size}px`
+            const left = `${w / 2 - size + size * i - (1 - 2 * i) * (w / 2 - size) * sf}px`
+            const top = `${h / 2 - size / 2}px`
+            return {
+                position,
+                left, 
+                top, 
+                width, 
+                height,
+                background 
+            }
+        }
+    }
+}
