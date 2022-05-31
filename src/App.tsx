1f0b02c295f75181458c57a81f3134e258a14bce
Anwesh43 <anweshthecool0@gmail.com>
2022-05-31 11:40:46 +0530

    using ButtonBreak component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ButtonBreak from './ButtonBreak';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonBreak></ButtonBreak>
+    </div>
+  );
+}
+
+export default App;
